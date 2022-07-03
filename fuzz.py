6b53caff064bae929215adc9865e93f3196fcce3
Johannes Riecken <johannes.riecken@gmail.com>
2022-07-03 17:28:31 +0200

    Add meaningless test

--- fuzz.py
+++ fuzz.py
@@ -5,7 +5,14 @@ with atheris.instrument_imports():
   import sys
 
 def TestOneInput(data):
-  P1.Task1.parse(data)
+  cap = 0 if len(data) == 0 else data[0] % 10
+  cache = P1.Task1.LRUCache(cap)
+  sets = 0 if len(data[1:]) == 0 else data[1] % 10
+  for i in range(sets):
+    cache.set(i, i)
+  gets = 0 if len(data[2:]) == 0 else data[2] % 10
+  for i in range(gets):
+    cache.get(i)
 
 atheris.Setup(sys.argv, TestOneInput)
 atheris.Fuzz()
