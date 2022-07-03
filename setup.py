be2348fb3bb652a6daf34a935ee8b0ee95f42a78
Johannes Riecken <johannes.riecken@gmail.com>
2022-07-03 19:38:23 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='udacity',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='rieckenj',
+    author_email='',
+    description=''
+)
